05973b318eff1340c3217c9c89b38ae5533a0b42
Marco Aurélio <marco7.m@gmail.com>
2023-01-03 03:21:08 -0300

    cmt

--- lua/test-plugin.lua
+++ lua/test-plugin.lua
@@ -1,7 +1,7 @@
 -- want libmodal (see libmodal readme)
-local _maps = {
-    ['u'] = function() vim.api.nvim_command('vs') end,
-}
+-- local _maps = {
+--     ['u'] = function() vim.api.nvim_command('vs') end,
+-- }
 
 return function()
 	require('libmodal').layer.new(
