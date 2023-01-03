e96f2e6b9b12d1dad7a3aad080b661ecc8240da8
Marco Aurélio <marco7.m@gmail.com>
2023-01-03 03:05:54 -0300

    cmt

--- lua/test-plugin.lua
+++ lua/test-plugin.lua
@@ -3,15 +3,13 @@ local _maps = {
     ['u'] = function() vim.api.nvim_command('vs') end,
 }
 
-return function()
-	require('libmodal').layer.enter(
-            {
-                n = {
-                    u = {
-                        rhs = 'hs',
-                        noremap = true
-                    }
-                }
-            }
-        )
-end
+return require('libmodal').layer.enter(
+{
+    n = {
+        u = {
+            rhs = 'hs',
+            noremap = true
+        }
+    }
+}
+)
